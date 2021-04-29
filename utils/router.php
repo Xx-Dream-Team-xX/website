d2470f42e042b9fc7ebd6a1b9e376255edbb0867
alecs297 <alexandru.nicolae.moldovan@eisti.fr>
2021-04-29 21:13:33 +0200

    comments

--- utils/router.php
+++ utils/router.php
@@ -2,10 +2,34 @@
 /**
  * Routing helper
  */
+
+    /**
+     * Router : Allows settings regex rules and wildcards for easy url redirections
+     */
     class Router {
+        /**
+         * Added routes list
+         *
+         * @var array routes
+         */
         private static $routes = [];
+
+        /**
+         * Default route (404)
+         *
+         * @var string
+         */
         private static $default = "/";
 
+        /**
+         * Adds a route to the routing list
+         *
+         * @param string $path URL (or regex)
+         * @param string $target Destination
+         * @param boolean $regex Self explanatory
+         * @param integer $permission Required permission level 
+         * @param boolean $wildcard Keeps or not the original request path (minus the first endpoint). ex: /a/b/c.d => /new/c.d
+         */
         public static function add($path, $target, $regex = false, $permission = 0, $wildcard = false) {
             array_push(self::$routes, [
                 "path" => $regex ? $path : "/^$path$/",
@@ -16,10 +40,20 @@
 
         }
 
+        /**
+         * Sets default route (404)
+         *
+         * @param string $target Destination
+         */
         public static function default($target) {
             self::$default = $target;
         }
 
+        /**
+         * Redirects traffic to the correct endpoint
+         *
+         * @param string $path Original URL (array form, split by "/")
+         */
         public static function start($path) {
             $found = false;
             foreach (self::$routes as $route) {
@@ -42,20 +76,35 @@
 
     }
     
+    /**
+     * Renders variable as JSON with correct content type then exits
+     *
+     * @param [any] $content variable to encode
+     */
     function send_json($content) {
         header('Content-Type: application/json');
         echo json_encode($content, JSON_PRETTY_PRINT);
 
     }
 
-    function render($page) {
-        if (is_file($page)) {
-            include($page);
+    /**
+     * Includes a file if it exists, errors otherwise
+     *
+     * @param [type] $path
+     */
+    function render($path) {
+
+        if (is_file($path)) {
+            include($path);
         } else {
             notfound();
         }
     }
 
+    /**
+     * Not found function
+     * 
+     */
     function notfound() {
         include(PATH["views"] . "error.php");
         exit();
