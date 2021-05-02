09b37f3131153f7fb01fea33f3f9986487b19352
alecs297 <alexandru.nicolae.moldovan@eisti.fr>
2021-05-02 21:13:37 +0200

    included user

--- pages/views/tests/testDB.php
+++ pages/views/tests/testDB.php
@@ -1,12 +1,14 @@
 <?php
-    // $user = UserType::createUserByType(array(
-    //     'id' => uniqid(),
-    //     'type' => UserType::USER,
-    //     'mail' => 'js1p@prout.fr',
-    //     'first_name' => "Roger",
-    //     'last_name' => "Amir"
-    // ));
-    // DB::writeNewUser(PATH['database'] . 'testDB.json', $user->getAll());
+
+    include_once(get_path('users.php'));
+    $user = UserType::createUserByType(array(
+        'id' => uniqid(),
+        'type' => UserType::USER,
+        'mail' => 'js1p@prout.fr',
+        'first_name' => "Roger",
+        'last_name' => "Amir"
+    ));
+    DB::writeNewUser(PATH['database'] . 'testDB.json', $user->getAll());
 
     send_json(DB::getUserByMail(PATH['database'] . "testDB.json","jsp@prout.fr"));
 ?>
