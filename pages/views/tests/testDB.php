170bfab28d023a98b60073bed3b7629b4dd9f28b
Samuel <sam200133@gmail.com>
2021-05-02 14:29:39 +0200

    Added test page for the DB

--- pages/views/tests/testDB.php
+++ pages/views/tests/testDB.php
@@ -1,3 +1,11 @@
 <?php
-
+    $db = new DataBase(PATH['database']. "test.json");
+    //send_json($db->getFullDBInfo()[]);
+    try {
+        $userdata = $db->getUserByID('fdsfkjg')->getAll();
+        send_json($userdata);
+    } catch (\Throwable $th) {
+        echo $th->getMessage();
+    }
+    //send_json($db->getUserByID('dfklhjk'));
 ?>
