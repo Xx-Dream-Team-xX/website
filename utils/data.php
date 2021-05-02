c22bfa889bc37c21191268ff799b04758c1d8363
Samuel <sam200133@gmail.com>
2021-05-02 14:30:31 +0200

    Can now read from a .json DB

--- utils/data.php
+++ utils/data.php
@@ -2,6 +2,85 @@
 /**
  * Database utils.
  */
+
+    include_once './utils/users.php';
+    /**
+     *  Class for accessing and managing the database
+     */
+    class DataBase {
+
+        /**
+         * Path of the database
+         *
+         * @var string
+         */
+        private $path = "";
+
+        private $data = array();
+
+        /**
+         * Constructor for the database class
+         *
+         * @param string $path to the database
+         */
+        public function __construct($path)
+        {
+            $this->path = $path;
+            if ($file = file_get_contents($this->path)) {
+                $this->data = json_decode($file, 1);
+            }
+        }
+
+        /**
+         * Querry every data from the database
+         *
+         * @return array return all database data and its $path
+         */
+        public function getFullDBInfo()
+        {
+            return array(
+                'data' => $this->data,
+                'path' => $this->path,
+            );
+        }
+
+        /**
+         * Querry user data from it's ID
+         *
+         * @param string $userId
+         * @return User Class instance of the user or throw an exception if not found
+         */
+        public function getUserByID($userId){
+            foreach ($this->data as $user) {
+                if ($user['id'] == $userId) {
+
+                    $user_object = new User($user['type'], $user['mail'], $user['first_name'], $user['last_name'], $user['id']);
+
+                    return $user_object;
+
+                    break;
+                }
+            }
+            throw new Exception("Unknown user with id : $userId.", 1);
+        }
+
+
+        public function getUserByMail($mail){
+            foreach ($this->data as $user) {
+                if ($user['mail'] == $mail) {
+
+                    $user_object = new User($user['type'], $user['mail'], $user['first_name'], $user['last_name'],$user['id']);
+
+                    return $user_object;
+
+                    break;
+                }
+            }
+            throw new Exception("Unknown user with e-mail : $mail.", 1);
+        }
+    }
+
+
     function getUser($user) { // placeholder
         return null;
     }
@@ -33,13 +112,5 @@
             'path' => $path,
         );
     }
-
-    $a = getDatabase(getDatabasePath('test'));
-
-    $b = array('712983' => array(
-        'name' => 'abc',
-    ));
-
-    setData(getDatabasePath('test'), $b);
 
 ?>
