74da4bbc9c653d8526ae1dc32d45e1240271cd6a
alecs297 <alexandru.nicolae.moldovan@eisti.fr>
2021-05-02 21:12:45 +0200

    renamed get_page to get_path because it gets paths

--- index.php
+++ index.php
@@ -14,13 +14,13 @@
     $path = explode('?', $url, 2)[0];
     $path_array = array_slice(explode('/', $path), 1);
 
-    Router::add('', get_page('views', 'index.php')); // Sans regex ni wildcard
-    Router::add('/^[0-9]+:[0-9]+$/', get_page('views', 'show.php'), true);
+    Router::add('', get_path('views', 'index.php')); // Sans regex ni wildcard
+    Router::add('/^[0-9]+:[0-9]+$/', get_path('views', 'show.php'), true);
 
     Router::add('static', PATH['static'], false, 0, $wildcard = true); // Sans regex, avec wildcard
 
-    Router::add('testDB', get_page('views', 'tests/testDB.php'));
+    Router::add('testDB', get_path('views', 'tests/testDB.php'));
 
-    Router::default(get_page('views', 'error.php'));
+    Router::default(get_path('views', 'error.php'));
     Router::start($path_array);
 ?>
