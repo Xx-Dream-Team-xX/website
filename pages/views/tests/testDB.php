87afedd2f420f444bceff736557e099abbe39a43
Rodrigues Samuel <sam200133@gmail.com>
2021-05-03 15:44:25 +0200

    dunno

--- pages/views/tests/testDB.php
+++ pages/views/tests/testDB.php
@@ -1,6 +1,7 @@
 <?php
 
     include_once get_path('utils', 'types_utils/users.php');
+    include_once get_path('utils', 'types_utils/contract.php');
     $user = UserType::createUserByType(array(
         'type' => UserType::ASSURE,
         'mail' => 'js2p@prout.fr',
@@ -8,7 +9,21 @@
         'last_name' => 'Roubin',
         'phone' => '+336-84-72-58-39',
     ));
-    DB::writeNewUser(PATH['database'] . 'testDB.json', $user->getAll());
+    DB::writeNewUser(PATH['database'] . 'testUsers.json', $user->getAll());
 
-    send_json(DB::getUserByMail(PATH['database'] . 'testDB.json', 'js2p@prout.fr'));
+    $contract = new Contract(array(
+        'owners' => array($user->getID()),
+        'start' => time(),
+        'end' => time(),
+        'vID' => 'AB123CD',
+        'contractID' => 454705435745,
+        'insurance' => '547596fjk543',
+        'countryCode' => 'F6579',
+        'category' => 'A',
+        'manufacturer' => 'Ford',
+    ));
+
+    DB::writeObject(PATH['database'] . 'testContract.json', $contract->getAll());
+    send_json(DB::getFromID(PATH['database'] . 'testContract.json', $contract->getID()));
+    //send_json(DB::getUserByMail(PATH['database'] . 'testUsers.json', 'js2p@prout.fr'));
 ?>
