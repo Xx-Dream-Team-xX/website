85af40e7f245b67a91e89451260a7c96f8991b9a
alecs297 <alexandru.nicolae.moldovan@eisti.fr>
2021-05-04 20:54:31 +0200

    added file

--- utils/auth.php
+++ utils/auth.php
@@ -2,4 +2,27 @@
 /**
  * User authentification.
  */
+    public class Auth {
+        $path = "";
+
+        public function checkPasswordFormat(string $pass) {
+
+        }
+
+        public function checkEmailFormat(string $email) {
+            
+        }
+
+        public function checkPhoneFormat(string $phone) {
+            
+        }
+
+        public function __construct(string $path) {
+            if (file_exists($path)) {
+                $this->path = $path;
+            } else {
+                throw new Exception("Specified database does not exist", 1);
+            }
+        }
+    }
 ?>
