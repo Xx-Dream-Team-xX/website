6e55ea240832221928d8f164f1e1bd828dee8bff
Rodrigues Samuel <sam200133@gmail.com>
2021-05-05 12:10:42 +0200

    refacto of the  class

--- utils/logs.php
+++ utils/logs.php
@@ -3,77 +3,79 @@
  * Logging utils.
  */
     /**
-     * Logger class, writes logs to files, has different verbose levels
+     * Logger class, writes logs to files, has different verbose levels.
      */
     class Logger {
-
         /**
-         * Logs directory path
-         *
-         * @var string
-         */
-        private static $basepath = '';
-
-        /**
-         * Verbose level
-         *
-         * @var integer
-         */
-        private static $level = 2;
-
-        /**
-         * Access level constants
+         * Access level constants.
          */
         public const ACCESS = 1;
+
         public const BASIC = 2;
+
         public const ACTIONS = 3;
+
         public const ADMIN = 4;
+
         public const ERRORS = 5;
 
         /**
-         * Current hour, minute and second
+         * Logs directory path.
+         *
+         * @var string
+         */
+        private $basepath = '';
+
+        /**
+         * Verbose level.
+         *
+         * @var int
+         */
+        private $level = 2;
+
+        /**
+         * Logger setup.
+         *
+         * @param string $path  Logging folder
+         * @param string $level Minimum verbose level
+         */
+        public function __construct(string $path, string $level) {
+            $this->basepath = $path;
+            $this->level = $level;
+
+            if (!is_dir($path) || (static::ERRORS < $level) || (static::ACCESS > $level)) {
+                throw new Exception('Invalid logger setup', 1);
+            }
+        }
+
+        /**
+         * Logs content, according to the verbose level.
+         *
+         * @param int    $level   Verbose level of current message
+         * @param string $content Message
+         */
+        public function log(int $level, string $content) {
+            if ($level >= static::$level) {
+                file_put_contents($this->today_file(), "[{$level}]" . self::now() . $content . "\n", FILE_APPEND);
+            }
+        }
+
+        /**
+         * Current hour, minute and second.
          *
          * @return string
          */
-        private function now() {
+        private static function now() {
             return date('[G:i:s] ');
         }
 
         /**
-         * Logging file path, with today's date in name
+         * Logging file path, with today's date in name.
          *
          * @return string
          */
         private function today_file() {
-            return static::$basepath . date('d-m-y') . "-logs.txt";
-        }
-
-        /**
-         * Logs content, according to the verbose level
-         *
-         * @param integer $level Verbose level of current message
-         * @param string $content Message
-         * @return void
-         */
-        public function log(int $level, string $content) {
-            if ($level >= static::$level) {
-                file_put_contents(static::today_file(), "[$level]" . static::now() . $content . "\n", FILE_APPEND);
-            }
-        }
-
-        /**
-         * Logger setup
-         *
-         * @param string $path Logging folder
-         * @param string $level Minimum verbose level
-         */
-        public function __construct(string $path, string $level) {
-            static::$basepath = $path;
-            static::$level = $level;
-
-            if (!is_dir($path) || (static::ERRORS < $level) || (static::ACCESS > $level)) {
-                throw new Exception("Invalid logger setup", 1);
-            }
+            return $this->basepath . date('d-m-y') . '-logs.txt';
         }
     }
 ?>
