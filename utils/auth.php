b7a3d1ecb279fe923d41343b9783a7a5faadd683
Rodrigues Samuel <sam200133@gmail.com>
2021-05-06 10:40:17 +0200

    Improve data validation

--- utils/auth.php
+++ utils/auth.php
@@ -3,94 +3,26 @@
  * User authentification.
  */
     class Auth {
-
         /**
-         * Path of the auth database
-         *
-         * @var string
-         */
-        private $path = "";
-
-        /**
-         * Error codes
+         * Error codes.
          */
         public const INVALID_PHONE = 1;
+
         public const INVALID_ADDRESS = 2;
+
         public const INVALID_NAME = 3;
+
         public const INVALID_EMAIL = 4;
 
         /**
-         * Regex check for specific type
+         * Path of the auth database.
          *
-         * @param string $pass
-         * @return void
+         * @var string
          */
-        private function checkPasswordFormat(string $pass) {
-            return true;
-        }
+        private $path = '';
 
         /**
-         * Regex check for specific type
-         *
-         * @param string $email
-         * @return void
-         */
-        private function checkEmailFormat(string $email) {
-            return true;
-        }
-
-        /**
-         * Regex check for specific type
-         *
-         * @param string $phone
-         * @return void
-         */
-        private function checkPhoneFormat(string $phone) {
-            return true;
-        }
-
-        /**
-         * Regex check for specific type
-         *
-         * @param string $name
-         * @return void
-         */
-        private function checkNameFormat(string $name) {
-            return true;
-        }
-
-        /**
-         * Regex check for specific type
-         *
-         * @param string $street
-         * @return void
-         */
-        private function checkStreetFormat(string $street) {
-            return true;
-        }
-
-        /**
-         * Regex check for specific type
-         *
-         * @param string $zip
-         * @return void
-         */
-        private function checkZipFormat(string $zip) {
-            return true;
-        }
-
-        /**
-         * Generates a password
-         *
-         * @param integer $l
-         * @return void
-         */
-        private function genPassword($l = 12) {
-            return random_bytes($l);
-        }
-
-        /**
-         * Initialization, sets auth path
+         * Initialization, sets auth path.
          *
          * @param string $path DB path
          */
@@ -98,57 +30,52 @@
             if (file_exists($path)) {
                 $this->path = $path;
             } else {
-                throw new Exception("Specified database does not exist", 1);
+                throw new Exception('Specified database does not exist', 1);
             }
         }
 
         /**
-         * Assigns user session to user account
+         * Logs in a user.
          *
-         * @param array $user Array of user data
-         * @return void
-         */
-        private function assign(array $user) {
-            $_SESSION["user"] = $user;
-        }
-
-        /**
-         * Logs in a user
-         *
-         * @param string $email provided email
+         * @param string $email    provided email
          * @param string $password provided password (plain text)
+         *
          * @return bool if success
          */
         public function login(string $email, string $password) {
-            if ($this->checkEmailFormat($email) && $this->checkPasswordFormat($password)) {
+            if (User::checkEmail($email) && $this->checkPasswordFormat($password)) {
                 $user = DB::getUserByMail($this->path, $email);
-                if ($user && password_verify($password, $user["password_hash"])) {
+                if ($user && password_verify($password, $user['password_hash'])) {
                     $this->assign($user);
+
                     return true;
                 }
             }
+
             return false;
         }
 
         /**
-         * Registers a new user, generates random password
+         * Registers a new user, generates random password.
          *
          * @param array $user user data array
+         *
          * @return array success and random password OR error and error code
          */
         public function register(array $user) {
             $error = 0;
-            if ($this->checkEmailFormat($user["mail"])) {
-                if ($this->checkNameFormat($user["first_name"]) && $this->checkNameFormat($user["last_name"])) {
-                    if ($this->checkStreetFormat($user["address"]) && $this->checkZipFormat($user["zip"])) {
-                        if ($this->checkPhoneFormat($user["phone"])) {
+            if (User::checkEmail($user['mail'])) {
+                if ($this->checkNameFormat($user['first_name']) && $this->checkNameFormat($user['last_name'])) {
+                    if ($this->checkStreetFormat($user['address']) && $this->checkZipFormat($user['zip'])) {
+                        if (User::checkPhone($user['phone'])) {
                             $password = $this->genPassword();
-                            $user["password"] = $password;
+                            $user['password'] = $password;
                             $user = User::createUserByType($user);
                             DB::setObject($this->path, $user->getAll(), true);
+
                             return array(
                                 'success' => true,
-                                'password' => $password
+                                'password' => $password,
                             );
                         }
                         $error = self::INVALID_PHONE;
@@ -161,8 +88,62 @@
 
             return array(
                 'success' => false,
-                'message' => $error
+                'message' => $error,
             );
+        }
+
+        /**
+         * Regex check for specific type.
+         */
+        private function checkPasswordFormat(string $pass) {
+            return true;
+        }
+
+        /**
+         * Regex check for specific type.
+         */
+        private function checkNameFormat(string $name) {
+            return true;
+        }
+
+        /**
+         * Regex check for specific type.
+         */
+        private function checkStreetFormat(string $street) {
+            return true;
+        }
+
+        /**
+         * Regex check for specific type.
+         */
+        private function checkZipFormat(string $zip) {
+            return true;
+        }
+
+        /**
+         * Generates a password.
+         *
+         * @param int $l
+         */
+        private function genPassword($l = 12) {
+            $alphabet = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890';
+            $pass = array(); //remember to declare $pass as an array
+            $alphaLength = strlen($alphabet) - 1; //put the length -1 in cache
+            for ($i = 0; $i < $l; ++$i) {
+                $n = rand(0, $alphaLength);
+                $pass[] = $alphabet[$n];
+            }
+
+            return implode($pass); //turn the array into a string
+        }
+
+        /**
+         * Assigns user session to user account.
+         *
+         * @param array $user Array of user data
+         */
+        private function assign(array $user) {
+            $_SESSION['user'] = $user;
         }
     }
 ?>
