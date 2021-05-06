489adcab71c1af0a762fa761c09cc3105a8a6668
alecs297 <alexandru.nicolae.moldovan@eisti.fr>
2021-05-06 23:29:35 +0200

    added get all utility (not fully yet)

--- endpoints/user.php
+++ endpoints/user.php
@@ -2,4 +2,23 @@
     /**
      * User information, list
      */
+
+    include_once get_path('utils', 'types_utils/users');
+
+    switch (get_final_point()) {
+        case 'list':
+            if (getPermissions() > User::GESTIONNAIRE) {
+                $users = DB::getAll(get_path('database', 'users.json'));
+                
+                if (getPermissions() === User::GESTIONNAIRE) {
+                    $users = array_filter($users, function($u) {
+                        return (($u["type"] === User::ASSURE) && ($u["rep"] === $_SESSION["user"]["assurance"]));
+                    });
+                }
+            }
+            break;
+        default:
+            # code...
+            break;
+    }
 ?>
