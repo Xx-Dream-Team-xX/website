d63de39c78d8518c1aaf3c11479dc4bc7aeab9bf
alecs297 <alexandru.nicolae.moldovan@eisti.fr>
2021-05-06 10:04:16 +0200

    comments

--- utils/auth.php
+++ utils/auth.php
@@ -4,41 +4,96 @@
  */
     class Auth {
 
+        /**
+         * Path of the auth database
+         *
+         * @var string
+         */
         private $path = "";
 
+        /**
+         * Error codes
+         */
         public const INVALID_PHONE = 1;
         public const INVALID_ADDRESS = 2;
         public const INVALID_NAME = 3;
         public const INVALID_EMAIL = 4;
 
+        /**
+         * Regex check for specific type
+         *
+         * @param string $pass
+         * @return void
+         */
         private function checkPasswordFormat(string $pass) {
             return true;
         }
 
+        /**
+         * Regex check for specific type
+         *
+         * @param string $email
+         * @return void
+         */
         private function checkEmailFormat(string $email) {
             return true;
         }
 
+        /**
+         * Regex check for specific type
+         *
+         * @param string $phone
+         * @return void
+         */
         private function checkPhoneFormat(string $phone) {
             return true;
         }
 
+        /**
+         * Regex check for specific type
+         *
+         * @param string $name
+         * @return void
+         */
         private function checkNameFormat(string $name) {
             return true;
         }
 
+        /**
+         * Regex check for specific type
+         *
+         * @param string $street
+         * @return void
+         */
         private function checkStreetFormat(string $street) {
             return true;
         }
 
+        /**
+         * Regex check for specific type
+         *
+         * @param string $zip
+         * @return void
+         */
         private function checkZipFormat(string $zip) {
             return true;
         }
 
+        /**
+         * Generates a password
+         *
+         * @param integer $l
+         * @return void
+         */
         private function genPassword($l = 12) {
             return random_bytes($l);
         }
 
+        /**
+         * Initialization, sets auth path
+         *
+         * @param string $path DB path
+         */
         public function __construct(string $path) {
             if (file_exists($path)) {
                 $this->path = $path;
@@ -47,10 +102,23 @@
             }
         }
 
+        /**
+         * Assigns user session to user account
+         *
+         * @param array $user Array of user data
+         * @return void
+         */
         private function assign(array $user) {
             $_SESSION["user"] = $user;
         }
 
+        /**
+         * Logs in a user
+         *
+         * @param string $email provided email
+         * @param string $password provided password (plain text)
+         * @return bool if success
+         */
         public function login(string $email, string $password) {
             if ($this->checkEmailFormat($email) && $this->checkPasswordFormat($password)) {
                 $user = DB::getUserByMail($this->path, $email);
@@ -62,6 +130,12 @@
             return false;
         }
 
+        /**
+         * Registers a new user, generates random password
+         *
+         * @param array $user user data array
+         * @return array success and random password OR error and error code
+         */
         public function register(array $user) {
             $error = 0;
             if ($this->checkEmailFormat($user["mail"])) {
