a0d88b4adfcd2915bea34640e893fae5fd902f20
alecs297 <alexandru.nicolae.moldovan@eisti.fr>
2021-05-06 09:58:26 +0200

    auth initial commmit

--- utils/auth.php
+++ utils/auth.php
@@ -3,18 +3,40 @@
  * User authentification.
  */
     class Auth {
+
         private $path = "";
 
-        public function checkPasswordFormat(string $pass) {
+        public const INVALID_PHONE = 1;
+        public const INVALID_ADDRESS = 2;
+        public const INVALID_NAME = 3;
+        public const INVALID_EMAIL = 4;
 
+        private function checkPasswordFormat(string $pass) {
+            return true;
         }
 
-        public function checkEmailFormat(string $email) {
-
+        private function checkEmailFormat(string $email) {
+            return true;
         }
 
-        public function checkPhoneFormat(string $phone) {
+        private function checkPhoneFormat(string $phone) {
+            return true;
+        }
 
+        private function checkNameFormat(string $name) {
+            return true;
+        }
+
+        private function checkStreetFormat(string $street) {
+            return true;
+        }
+
+        private function checkZipFormat(string $zip) {
+            return true;
+        }
+
+        private function genPassword($l = 12) {
+            return random_bytes($l);
         }
 
         public function __construct(string $path) {
@@ -23,6 +45,50 @@
             } else {
                 throw new Exception("Specified database does not exist", 1);
             }
+        }
+
+        private function assign(array $user) {
+            $_SESSION["user"] = $user;
+        }
+
+        public function login(string $email, string $password) {
+            if ($this->checkEmailFormat($email) && $this->checkPasswordFormat($password)) {
+                $user = DB::getUserByMail($this->path, $email);
+                if ($user && password_verify($password, $user["password_hash"])) {
+                    $this->assign($user);
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public function register(array $user) {
+            $error = 0;
+            if ($this->checkEmailFormat($user["mail"])) {
+                if ($this->checkNameFormat($user["first_name"]) && $this->checkNameFormat($user["last_name"])) {
+                    if ($this->checkStreetFormat($user["address"]) && $this->checkZipFormat($user["zip"])) {
+                        if ($this->checkPhoneFormat($user["phone"])) {
+                            $password = $this->genPassword();
+                            $user["password"] = $password;
+                            $user = User::createUserByType($user);
+                            DB::setObject($this->path, $user->getAll(), true);
+                            return array(
+                                'success' => true,
+                                'password' => $password
+                            );
+                        }
+                        $error = self::INVALID_PHONE;
+                    }
+                    $error = self::INVALID_ADDRESS;
+                }
+                $error = self::INVALID_NAME;
+            }
+            $error = self::INVALID_EMAIL;
+
+            return array(
+                'success' => false,
+                'message' => $error
+            );
         }
     }
 ?>
