9831d2ef3671379eadb7d3fc25b7d5f49f29284a
alecs297 <alexandru.nicolae.moldovan@eisti.fr>
2021-05-12 16:37:25 +0200

    logoff, change password and partial register

--- endpoints/auth.php
+++ endpoints/auth.php
@@ -1,6 +1,7 @@
 <?php
+
     /**
-     * Login, registration, account security, logoff
+     * Login, registration, account security, logoff.
      */
     include_once get_path('utils', 'auth.php');
 
@@ -8,21 +9,26 @@
 
     switch (get_final_point()) {
         case 'login':
-            send_json(isset($_POST["login"], $_POST["password"]) && $auth->login($_POST["login"], $_POST["password"]));
+            send_json(isset($_POST['login'], $_POST['password']) ? $auth->login($_POST['login'], $_POST['password']) : false);
+
             break;
         case 'register':
-            # code...
+            if (getPermissions() > 2) {
+                send_json($auth->register($_POST)); // Utilisateur lambda, à faire par types
+            }
             break;
         case 'logoff':
             session_destroy();
+
             break;
         case 'changepassword':
             if (getPermissions() > 0) {
-                
+                send_json(isset($_POST['password']) ? $auth->changePassword($_SESSION['user']['id'], $_SESSION['user']['mail'], $_POST['password'], $_POST['new'] ?? null) : false);
             }
+
             break;
         default:
-            # code...
+            notfound();
             break;
     }
 ?>
