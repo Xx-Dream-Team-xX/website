5b624bc249eec046551291c08aad866303f52d28
alecs297 <alexandru.nicolae.moldovan@eisti.fr>
2021-05-17 17:06:50 +0200

    password format check

--- utils/auth.php
+++ utils/auth.php
@@ -147,7 +147,7 @@ class Auth {
      * Regex check for specific type.
      */
     private function checkPasswordFormat(?string $pass) {
-        return true;
+        return preg_match("/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/gm", $pass);
     }
 
     /**
