7588dc61262f3a75b2516f8bcfe118741d87ae4c
alecs297 <alexandru.nicolae.moldovan@eisti.fr>
2021-05-17 19:51:36 +0200

    added utility

--- utils/permissions.php
+++ utils/permissions.php
@@ -4,12 +4,33 @@
  *
  * @param null|mixed $user
  */
+
+    function isLoggedIn() {
+        return (isset($_SESSION, $_SESSION['user']));
+    }
+
     function getPermissions($user = null) {
         $perm = 0;
-        if (isset($_SESSION, $_SESSION['user'])) {
+        if (isLoggedIn()) {
             $perm = (int) $_SESSION['user']['type'];
         }
 
         return $perm;
+    }
+
+    function getId() {
+        if (isLoggedIn()) {
+            return $_SESSION['user']['id'];
+        } else {
+            return false;
+        }
+    }
+
+    function getMail() {
+        if (isLoggedIn()) {
+            return $_SESSION['user']['mail'];
+        } else {
+            return false;
+        }
     }
 ?>
