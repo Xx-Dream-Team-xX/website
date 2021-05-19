0b1ff98af522a95f1e81ab2eaa540a8f5ee77645
alecs297 <alexandru.nicolae.moldovan@eisti.fr>
2021-05-19 11:44:57 +0200

    using function

--- endpoints/user.php
+++ endpoints/user.php
@@ -14,54 +14,9 @@
                 $filter = null;
                 $map = null;
 
-                switch (getPermissions()) {
-                    case User::GESTIONNAIRE:
-                        $filter = function($u) {
-                            return (($u["type"] === User::ASSURE) && ($u["assurance"] === $_SESSION["user"]["assurance"]));
-                        };
-                        $map = function($u) {
-
-                            return array(
-                                'id' => $u["id"],
-                                'name' => $u["last_name"] . " " . $u["first_name"],
-                                'mail' => $u["mail"],
-                                'type' => $u["type"],
-                                'birth' => $u["birth"],
-                                'declarations' => sizeof($u["declarations"]),
-                                'contracts' => sizeof($u["contracts"]),
-                                'sinisters' => sizeof($u["sinisters"]),
-                                'actions' => sizeof($u["actions"])
-                            );
-                        };
-                        break;
-                    case User::ADMIN:
-                        $filter = function($u) {
-                            return ($u["type"] !== User::ADMIN);
-                        };
-                        $map = function($u) {
-                            $u = (User::createUserByType($u))->getPublic();
-                            return ;
-                        };
-                        break;
-                    case User::ASSURE:
-                        $filter = function($u) {
-                            return (($u["type"] === User::GESTIONNAIRE) && ($u["assurance"] === $_SESSION["user"]["assurance"]));
-                        };
-                        $map = function($u) {
-                            return array(
-                                'id' => $u['id'],
-                                'name' => $u["last_name"] . " " . $u["first_name"],
-                                'mail' => ($u["id"]===$_SESSION["user"]["rep"]) ? $u["mail"] : false
-                            );
-                        };
-                        break;
-                    default:
-                        $users = [];
-                        
-                        break;
-                }
+                $fcts = User::getInteractions();
                 
-                send_json(array_map($map, array_filter($users, $filter)));
+                send_json(array_map($fcts[1], array_filter($users, $fcts[0])));
             }
             break;
         default:
