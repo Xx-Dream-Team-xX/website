dc033b546cb4de5e6a2f2cae8b48cfac1fa37e4b
alecs297 <alexandru.nicolae.moldovan@eisti.fr>
2021-05-19 11:44:20 +0200

    filter

--- utils/types_utils/users.php
+++ utils/types_utils/users.php
@@ -334,6 +334,69 @@
                     break;
             }
         }
+
+        /**
+         * Gets interactionnable users list callbacks for map and filter
+         *
+         * @return array [filter, map] callbacks
+         */
+        public static function getInteractions() {
+            $filter = null;
+            $map = null;
+
+            switch (getPermissions()) {
+                case User::GESTIONNAIRE:
+
+                    $filter = function($u) {
+
+                        return (($u["type"] === User::ASSURE) && ($u["assurance"] === $_SESSION["user"]["assurance"]));
+                    };
+
+                    $map = function($u) {
+
+                        return array(
+                            'id' => $u["id"],
+                            'name' => $u["last_name"] . " " . $u["first_name"],
+                            'mail' => $u["mail"],
+                            'type' => $u["type"],
+                            'birth' => $u["birth"],
+                            'declarations' => sizeof($u["declarations"]),
+                            'contracts' => sizeof($u["contracts"]),
+                            'sinisters' => sizeof($u["sinisters"]),
+                            'actions' => sizeof($u["actions"])
+                        );
+                    };
+                    break;
+                case User::ADMIN:
+                    $filter = function($u) {
+                        return ($u["type"] !== User::ADMIN);
+                    };
+                    $map = function($u) {
+                        $u = (User::createUserByType($u))->getPublic();
+                        return ;
+                    };
+                    break;
+                case User::ASSURE:
+                    $filter = function($u) {
+                        return (($u["type"] === User::GESTIONNAIRE) && ($u["assurance"] === $_SESSION["user"]["assurance"]));
+                    };
+                    $map = function($u) {
+                        return array(
+                            'id' => $u['id'],
+                            'name' => $u["last_name"] . " " . $u["first_name"],
+                            'mail' => ($u["id"]===$_SESSION["user"]["rep"]) ? $u["mail"] : false
+                        );
+                    };
+                    break;
+                default:
+                $filter = function($u) {
+                    return false;
+                };
+                    break;
+            }
+
+            return [$filter, $map];
+        }
     }
 
     /**
