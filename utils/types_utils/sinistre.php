02635cc10d629773204cf3ba90c6f272589cc217
Rodrigues Samuel <sam200133@gmail.com>
2021-05-23 00:36:16 +0200

    sinistre should be fully functioning

--- utils/types_utils/sinistre.php
+++ utils/types_utils/sinistre.php
@@ -59,6 +59,30 @@
                 'type' => 'text',
                 'optional' => true,
             ),
+            'garage_phone' => array(
+                'type' => 'phone',
+                'optional' => true,
+            ),
+            'garage_email' => array(
+                'type' => 'email',
+                'optional' => true,
+            ),
+            'garage_alt_phone' => array(
+                'type' => 'phone',
+                'optional' => true,
+            ),
+            'other_damage' => array(
+                'type' => 'text',
+                'optional' => true,
+            ),
+            'injureds' => array(
+                'type' => 'array',
+                'optional' => true,
+            ),
+            'constat' => array(
+                'type' => 'array',
+                'optional' => true,
+            ),
         );
 
         protected $id = '';
@@ -115,7 +139,10 @@
         public function __construct(array $rawSinistre) {
             if ($rawSinistre = validateObject($rawSinistre, self::$required)) {
                 if (isset($rawSinistre['injureds'])) {
-                    self::validateInjured($rawSinistre['injureds']);
+                    $rawSinistre['injureds'] = self::validateInjured($rawSinistre['injureds']);
+                }
+                if (isset($rawSinistre['constat'])) {
+                    $rawSinistre['constat'] = self::validateConstat($rawSinistre['constat']);
                 }
                 send_json($rawSinistre);
             } else {
@@ -123,38 +150,190 @@
             }
         }
 
-        private static function validateInjured(array &$injured_list) {
-            $required = array(
-                'firstname' => array(
-                    'type' => 'text',
-                ),
-                'lastname' => array(
-                    'type' => 'text',
-                ),
-                'address' => array(
-                    'type' => 'date',
-                ),
-                'phone' => array(
-                    'type' => 'phone',
-                ),
-                'profession' => array(
-                    'type' => 'text',
-                ),
-                'belt' => array(
-                    'type' => 'bool',
-                ),
-                'injuries' => array(
-                    'type' => 'text',
-                ),
+        /**
+         * Validate injured list.
+         *
+         * @return array|bool false if not valid
+         */
+        private static function validateInjured(array $injured_list) {
+            try {
+                $required = array(
+                    'firstname' => array(
+                        'type' => 'text',
+                    ),
+                    'lastname' => array(
+                        'type' => 'text',
+                    ),
+                    'address' => array(
+                        'type' => 'text',
+                    ),
+                    'phone' => array(
+                        'type' => 'phone',
+                    ),
+                    'profession' => array(
+                        'type' => 'text',
+                    ),
+                    'belt' => array(
+                        'type' => 'bool',
+                    ),
+                    'injuries' => array(
+                        'type' => 'text',
+                    ),
 
-            );
-            foreach ($injured_list as &$injured) {
-                if (!$injured = validateObject($injured, $required)) {
-                    return false;
+                );
+                foreach ($injured_list as &$injured) {
+                    $injured = validateObject($injured, $required);
                 }
-            }
 
-            return $injured_list;
+                return $injured_list;
+            } catch (Exception $e) {
+                throw new Exception("Injured list -> {$e->getMessage()}", 1);
+            }
+        }
+
+        private static function validateConstat(array $constat) {
+            try {
+                $required = array(
+                    'date' => array(
+                        'type' => 'date',
+                    ),
+                    'location' => array(
+                        'type' => 'text',
+                    ),
+                    'injured' => array(
+                        'type' => 'bool',
+                    ),
+                    'other_vehicle_involved' => array(
+                        'type' => 'bool',
+                    ),
+                    'other_object_involved' => array(
+                        'type' => 'bool',
+                    ),
+                    'wintensses' => array(
+                        'type' => 'text',
+                    ),
+                    'vehicle_A' => array(
+                        'type' => 'array',
+                    ),
+                    'vehicle_B' => array(
+                        'type' => 'array',
+                    ),
+                );
+                $constat = validateObject($constat, $required);
+                $constat['vehicle_A'] = self::validateConstatVehicle($constat['vehicle_A']);
+                $constat['vehicle_B'] = self::validateConstatVehicle($constat['vehicle_B']);
+
+                return $constat;
+            } catch (Exception $e) {
+                throw new Exception("Constat -> {$e->getMessage()}", 1);
+            }
+        }
+
+        private static function validateConstatVehicle(array $Vehcile) {
+            try {
+                $required = array(
+                    'user' => array(
+                        'type' => 'text',
+                    ),
+                    'contract' => array(
+                        'type' => 'text',
+                    ),
+                    'assurance' => array(
+                        'type' => 'text',
+                    ),
+                    'driver_firstname' => array(
+                        'type' => 'text',
+                    ),
+                    'driver_lastname' => array(
+                        'type' => 'text',
+                    ),
+                    'driver_birthdate' => array(
+                        'type' => 'date',
+                    ),
+                    'driver_address' => array(
+                        'type' => 'text',
+                    ),
+                    'driver_country' => array(
+                        'type' => 'text',
+                    ),
+                    'driver_phone' => array(
+                        'type' => 'phone',
+                    ),
+                    'dirver_email' => array(
+                        'type' => 'email',
+                    ),
+                    'driver_liscence_id' => array(
+                        'type' => 'text',
+                    ),
+                    'driver_liscence_cat' => array(
+                        'type' => 'text',
+                    ),
+                    'driver_liscence_expire' => array(
+                        'type' => 'date',
+                    ),
+                    'vehicle_initial_impact' => array(
+                        'type' => 'text',
+                    ),
+                    'vehicle_damage' => array(
+                        'type' => 'text',
+                    ),
+                    'observation' => array(
+                        'type' => 'text',
+                    ),
+                    'stationing' => array(
+                        'type' => 'bool',
+                    ),
+                    'leaving_parking_spot' => array(
+                        'type' => 'bool',
+                    ),
+                    'entering_parking_spot' => array(
+                        'type' => 'bool',
+                    ),
+                    'entering_place' => array(
+                        'type' => 'bool',
+                    ),
+                    'leaving_place' => array(
+                        'type' => 'bool',
+                    ),
+                    'round_point' => array(
+                        'type' => 'bool',
+                    ),
+                    'rear_damage_on_road' => array(
+                        'type' => 'bool',
+                    ),
+                    'oposite_road_line' => array(
+                        'type' => 'bool',
+                    ),
+                    'line_change' => array(
+                        'type' => 'bool',
+                    ),
+                    'overtake' => array(
+                        'type' => 'bool',
+                    ),
+                    'right_turn' => array(
+                        'type' => 'bool',
+                    ),
+                    'left_turn' => array(
+                        'type' => 'bool',
+                    ),
+                    'driving_backward' => array(
+                        'type' => 'bool',
+                    ),
+                    'past_line' => array(
+                        'type' => 'bool',
+                    ),
+                    'from_right' => array(
+                        'type' => 'bool',
+                    ),
+                    'skip_priority' => array(
+                        'type' => 'bool',
+                    ),
+                );
+
+                return validateObject($Vehcile, $required);
+            } catch (Exception $e) {
+                throw new Exception("Vehicle -> {$e->getMessage()}", 1);
+            }
         }
     }
 
