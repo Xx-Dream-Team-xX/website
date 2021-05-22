88cfc5dd72b3089a1f22717ed5fc810bd6f53913
alecs297 <alexandru.nicolae.moldovan@eisti.fr>
2021-05-22 11:58:55 +0200

    added timezone support

--- config/settings.php
+++ config/settings.php
@@ -3,11 +3,14 @@
  * Global settings.
  */
 
+ // Branding and URL settings
  define('SETTINGS', array(
      'name' => 'iCar',
      'version' => '0.0.1',
      'url' => 'http://localhost/'
  ));
+// Time zone settings, see https://www.php.net/manual/en/timezones.php
+ date_default_timezone_set('Europe/Paris');
  // List of possible path were web server files are stored for router to look into
  define('PATH', array(
      'api' => SERVER_ROOT . '/endpoints/',
