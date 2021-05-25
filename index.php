827ef29bb657eed24b07d9024fa140b2dd8fc7d5
alecs297 <alexandru.nicolae.moldovan@eisti.fr>
2021-05-25 17:54:48 +0200

    account endpoint

--- index.php
+++ index.php
@@ -29,6 +29,7 @@
     Router::add('users', get_path('api', 'user.php'));
     Router::add('contract', get_path('api', 'contract.php'));
     Router::add('auth', get_path('api', 'auth.php'));
+    Router::add('account', get_path('api', 'account.php'));
     Router::add('conversation', get_path('api', 'messages.php'));
     Router::add('ticket', get_path('api', 'tickets.php'));
     Router::add('notifications', get_path('api', 'notifications.php'));
