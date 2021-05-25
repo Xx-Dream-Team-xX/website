a6e7068ea861f2c03e8ad55198660ae201dc3767
alecs297 <alexandru.nicolae.moldovan@eisti.fr>
2021-05-25 16:08:29 +0200

    get user by id

--- endpoints/user.php
+++ endpoints/user.php
@@ -20,6 +20,42 @@
                 send_json(array_map($fcts[1], $users));
             }
             break;
+        
+        case 'get':
+            if (isset($_POST["id"]) && (strlen($_POST["id"]) === 13)) {
+
+                $t = DB::getFromID(get_path("database", "users.json"), $_POST["id"]);
+
+                if ($t && ($t = User::createUserByType($t))) {
+                    switch (getPermissions()) {
+                        case User::ADMIN:
+                            send_json($t->getPublic());
+                            break;
+                        case User::POLICE:
+                            $tmp = $t->getPublic();
+                            unset($tmp['actions']);
+                            unset($tmp['contracts']);
+                            unset($tmp['sinisters']);
+                            unset($tmp['declarations']);
+                            send_json($tmp);
+                            break;
+                        case User::GESTIONNAIRE:
+                            if (($t->getType() === User::ASSURE) && ($t->getAssurance() === getUpdatedUser()['assurance'])) {
+                                send_json($t->getPublic());
+                            } else {
+                                send_json(array(
+                                    'name' => $t->getName()[0] . " " . $t->getName()[1],
+                                    'type' => $t->getType()
+                                ));
+                            }
+                            break;
+                        default:
+                            break;
+                    }
+                }
+            }
+            break;
+            
         default:
             notfound();
             break;
