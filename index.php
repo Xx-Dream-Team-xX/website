01324a33d5943642aef2b8fcfe8279e0ce258ec8
LAMagicx <samsgrice@gmail.com>
2021-05-26 15:05:44 +0200

    fine lol

--- index.php
+++ index.php
@@ -15,7 +15,7 @@
     $path = explode('?', $url, 2)[0];
     $path_array = array_slice(explode('/', $path), 1);
 
-    Router::add('', get_path('views', 'index.php')); // Sans regex ni wildcard
+    Router::add('', get_path('views', 'mainpage.html')); // Sans regex ni wildcard
     Router::add('/^[0-9]+:[0-9]+$/', get_path('views', 'show.php'), true);
 
     Router::add('static', get_path('static'), false, 0, $wildcard = true); // Sans regex, avec wildcard
