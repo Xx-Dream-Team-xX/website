136da7e5501085123381572fb3039e0ae3f6c63f
LAMagicx <samsgrice@gmail.com>
2021-05-28 01:43:08 +0200

    reverted to older manageMessages.js

--- pages/static/js/manageMessages.js
+++ pages/static/js/manageMessages.js
@@ -15,7 +15,6 @@ function onLoad(){
         setInterval(() =>
             {
                 requestMessagesList()
-                getRecipients()
             }, 5000
         );
     });
@@ -42,7 +41,7 @@ function fill(input, data, filter="") {
         }
         return (a < b)
     })
-    data = data.filter(a => (a.name.includes(filter)));
+    data = data.filter(a => (a.name.toLowerCase().includes(filter.toLocaleLowerCase())));
     input.innerHTML = "";
     data.forEach(user => {
         el = document.createElement("option");
@@ -106,23 +105,22 @@ function requestMessagesList() {
             CACHE["messages"] = JSON.parse(this.responseText) ?? [];
             if (!CACHE['selected']) requestMessages(CACHE["messages"][CACHE["messages"].length - 1]["id"]);
             showRecentMessages(CACHE["messages"]);
-            highlight();
+            
         }
     }
 }
 
-function highlight(el) {
+function highlight() {
     
     els = document.querySelectorAll('.bg-success');
     els.forEach(e => {
         e.classList.remove('bg-success');
     });
 
-    if (el) el.classList.add("bg-success");
-    else CACHE["selected"] ? document.getElementById(CACHE["selected"]).classList.add("bg-success") : document.getElementById("recent").children[0].classList.add("bg-success");
+    CACHE["selected"] ? document.getElementById(CACHE["selected"]).classList.add("bg-success") : document.getElementById("recent").children[0].classList.add("bg-success");
 }
 
-function requestMessages(id, el) {
+function requestMessages(id) {
     let req = new XMLHttpRequest();
     let d = new FormData();
 
@@ -136,8 +134,6 @@ function requestMessages(id, el) {
             CACHE["selected"] = id;
             showMessages(CACHE["actual"]);
         }
-
-        if (el) highlight(el)
     }
     
 }
@@ -149,6 +145,7 @@ function showRecentMessages(DATA){
     for (let i = 0; i < DATA.length; i++) {
         getData(DATA[i]);
     }
+    highlight();
 }
 
 function showMessages(DATA) { 
@@ -159,6 +156,7 @@ function showMessages(DATA) {
 
     m = document.getElementById("messages");
     m.scrollTop = m.scrollHeight;
+    highlight()
 }
 
 function getMessage(DATA) {
@@ -237,7 +235,9 @@ function addConvtoRecent(id, type, people, content, sender, files, timestamp, un
         }
         
     };
-    if (id === CACHE["selected"]) requestMessages(id);
+    if (id === CACHE["selected"] && unread) {
+        requestMessages(id);
+    }
 
     a1.setAttribute("id", id);
     a1.setAttribute("onclick", "requestMessages(this.id, this)");
@@ -252,7 +252,7 @@ function addConvtoRecent(id, type, people, content, sender, files, timestamp, un
     i3.setAttribute("width", "50");
 
     let d4 = document.createElement('div');
-    d4.classList.add("media-body", "ml-4", "chat-message");
+    d4.classList.add("media-body", "ml-4");
     
     let d5 = document.createElement('div');
     d5.classList.add("d-flex", "align-items-center", "justify-content-between", "mb-1", "text-dark");
@@ -286,7 +286,7 @@ function addSenderMessage(id, sender, content, files, timestamp) {
     let message_box = document.getElementById("messages");
 
     let d1 = document.createElement('div');
-    d1.classList.add("media", "chat-media");
+    d1.classList.add("media", "w-50", "mb-3");
 
     let i2 = document.createElement('img');
     i2.classList.add("rounded-circle");
@@ -320,7 +320,7 @@ function addReveiverMessage(id, sender, content, files, timestamp) {
     let message_box = document.getElementById("messages");
 
     let d1 = document.createElement('div');
-    d1.classList.add("media", "chat-media");
+    d1.classList.add("media", "w-50", "mb-3", "ml-auto");
 
     let d3 = document.createElement('div');
     d3.classList.add("media-body");
@@ -392,11 +392,10 @@ function newConversation(id, content, files) {
     r.onreadystatechange = function() {
         if (this.status === 200 && this.readyState === 4) {
             if (id = JSON.parse(this.responseText)["id"]) {
+
                 requestMessagesList();
                 requestMessages(id);
                 document.getElementById("new_message").value = "";
-
-
             }
         }
     }
