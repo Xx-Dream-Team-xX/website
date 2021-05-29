404a963a2dac6cc892af87964ddb9cde12f6dc43
LAMagicx <samsgrice@gmail.com>
2021-05-29 16:34:33 +0200

    more css

--- pages/views/error.php
+++ pages/views/error.php
@@ -10,15 +10,33 @@
         <?php include(get_path("partials", "head.php")); ?>
         <script src="/static/js/manageLogin.js" charset="utf-8"></script>
     </head>
-    <body>
-        <div class="top"></div>
+    <script charset="utf-8">
+        function move() {
+            let a = document.getElementById("find-me");
+            let t = document.getElementById("t").getBoundingClientRect();
+            a.style.top = Math.floor(Math.random() * t.width - 50) + "px";
+            a.style.left = Math.floor(Math.random() * t.width - 50) + "px";
+        }
+    </script> 
+    <body onload="setTimeout(() => {  move(); }, 100);">
+        <a id="find-me" href="/" onclick="logout()"></a>
+        <div class="top" id="t" onclick="move()"></div>
         <img src="https://imgs.xkcd.com/comics/not_available.png" alt="404"/>
-        <div class="bottom"></div>
+        <div class="bottom" onclick="move()"></div>
     </body>
     <style type="text/css" media="screen">
         div {
             width: 100vw;
             height: 50vh;
+        }
+
+        a {
+            width: 50px;
+            height: 50px;
+            background-color: rgba(255,0,0,0);
+            display: block;
+            position: fixed;
+
         }
 
         img {
