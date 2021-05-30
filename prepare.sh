da8614073e70d523e5a8c9f6bc894ff7c1b3d3a8
Samuel <sam200133@gmail.com>
2021-05-30 22:55:43 +0200

    added scripts to easelly prepare and start server

---
+++ prepare.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+mkdir data
+mkdir data/uploads
+mkdir data/tickets
+mkdir data/messages
+
+mkdir logs
+
+touch data/assurances.json
+touch data/contracts.json
+touch data/conversations.json
+touch data/tickets.json
+touch data/sell.json
+touch data/sinistres.json
+touch data/users.json
